31a52719fdc7b9b3c3b2c455c6513e775c79cdec
Vinspi <vinspi13@gmail.com>
2019-02-11 19:14:17 +0100

    refactor models and add GSA_checks

---
+++ gsa-server/src/main/java/fr/uniamu/ibdm/gsa_server/models/Product.java
@@ -0,0 +1,36 @@
+package fr.uniamu.ibdm.gsa_server.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Product implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long productId;
+  private String productName;
+
+
+  public Product() {
+  }
+
+  public Long getProductId() {
+    return productId;
+  }
+
+  public void setProductId(Long productId) {
+    this.productId = productId;
+  }
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+}
