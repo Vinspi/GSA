b1937c9239798925e69c9f359231e18ef87a894e
Vinspi <vinspi13@gmail.com>
2019-02-13 22:25:15 +0100

    reupload JsonResponse

---
+++ gsa-server/src/main/java/fr/uniamu/ibdm/gsa_server/requests/JsonResponse.java
@@ -0,0 +1,68 @@
+package fr.uniamu.ibdm.gsa_server.requests;
+
+import java.io.Serializable;
+
+public class JsonResponse<T> implements Serializable {
+
+  protected RequestStatus status;
+  protected String error;
+  protected T data;
+
+  /**
+   *  Create JsonResponse object and set up status and data.
+   * @param status status of the response.
+   * @param data data to be sent in the response.
+   */
+  public JsonResponse(RequestStatus status, T data) {
+    this.status = status;
+    this.data = data;
+    this.error = null;
+  }
+
+  /**
+   * Create JsonResponse and set up status,
+   * data sent with the response will be empty javascript object.
+   * @param status status of the response.
+   */
+  public JsonResponse(RequestStatus status) {
+    this.status = status;
+    this.error = null;
+    this.data = null;
+  }
+
+  /**
+   * Create JsonResponse object and initialize it with status and error,
+   * the response will be empty javascript object.
+   * @param status status of the response.
+   * @param error error message.
+   */
+  public JsonResponse(String error, RequestStatus status) {
+    this.status = status;
+    this.error = error;
+    this.data = null;
+  }
+
+  public RequestStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(RequestStatus status) {
+    this.status = status;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+
+  public T getData() {
+    return data;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+}
