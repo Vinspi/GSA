8472b4cd09858fe5d0e47a65fe7b5eeffc2b041d
siiraaj <siiraaj@yahoo.fr>
2019-02-19 15:37:59 +0100

    update form registration

--- gsa-client/src/app/components/add-aliquote/add-aliquote.component.ts
+++ gsa-client/src/app/components/add-aliquote/add-aliquote.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { aliquoteOverview } from '../../aliquoteOverview';
+import { Subject } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-add-aliquote',
@@ -7,9 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAliquoteComponent implements OnInit {
 
-  constructor() { }
+  data: Array<aliquoteOverview>;
+  dtTrigger: Subject<any> = new Subject();
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getAllSpeciesName().subscribe(response => {
+      this.data = <Array<aliquoteOverview>> response.data;
+      console.log("data : "+JSON.stringify(this.data[0]));
+      this.dtTrigger.next();
+    });
   }
 
 }
